0754fcf121f202eb321c1c6d15facf0486ef83e1
Jay Brown <jlb1504@gmail.com>
2014-07-05 21:21:38 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JRubyTest2::Application.config.secret_key_base = 'ca261eba2e47db354e857b2365640cd1b1bbfd369d92375619f1882aa2f0a62fbff0083445a75b19d4d55a369b9b4d54b285732808b326c0e168d9068d429caa'
